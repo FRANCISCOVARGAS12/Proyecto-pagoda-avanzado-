cda63a57afc918a4fecc05af498787b2ab1e9c3f
Francisco Vargas Ramos <panchovr13@gmail.com>
2026-04-28 21:37:26 -0600

    Fix cors

--- pagoda-api/src/main/java/com/pagoda/pagoda_api/config/WebSocketConfig.java
+++ pagoda-api/src/main/java/com/pagoda/pagoda_api/config/WebSocketConfig.java
@@ -19,13 +19,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry
-            .addEndpoint("/ws-pagoda")
-            .setAllowedOrigins(
-                "http://localhost:4200",
-                "http://localhost:5173",
-                "https://franciscovargas12.github.io",
-                "https://paquinhodevv.github.io"
-            )
-            .withSockJS();
+                .addEndpoint("/ws-pagoda")
+                .setAllowedOriginPatterns("*") // Permite la conexión desde GitHub Pages sin importar la URL exacta
+                .withSockJS();
     }
 }
